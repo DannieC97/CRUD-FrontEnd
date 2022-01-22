0eaf1af7529faa508869794cc873a1f21d8d38f2
DannieC97 <danielchabi97@gmail.com>
2022-01-22 17:52:09 -0500

    homepage text

--- crud/src/components/Home.js
+++ crud/src/components/Home.js
@@ -1,12 +1,13 @@
 
-export default function Home(){
+export default function Home() {
 
-    return(
+    return (
         <div className="homeImage">
             {/* <img className="homeImg" src="http://www.brooklyn.cuny.edu/web/off_dosa/BCGRAD2020-Library-06_1920x1080.jpg" /> */}
-           <div className="title">
-            <h1>CRUD App</h1>
-            <h3>By: Daniel Chabi, Rivka Bar-Chaim, Vincent Lau </h3>
+            <div className="title">
+                <h1>CRUD App</h1>
+                <h3>By: Daniel Chabi, Rivka Bar-Chaim, Vincent Lau </h3>
+
             </div>
         </div>
     )
